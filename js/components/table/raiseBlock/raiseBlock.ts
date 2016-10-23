22f865726a758bfee084fc275d77126f80e52378
Andrey Kurdyumov <kant2002@gmail.com>
2016-10-23 13:22:26 +0600

    Add support for force update of raise block

--- js/components/table/raiseBlock/raiseBlock.ts
+++ js/components/table/raiseBlock/raiseBlock.ts
@@ -6,7 +6,16 @@ import { TableSlider } from "../../../table/tableSlider";
 export class RaiseBlockComponent {
     private tableSlider: TableSlider;
 
-    constructor(params: { data: TableSlider }) {
+    constructor(params: { data: TableSlider, updateTranslatorTrigger: KnockoutSubscribable<any>, updateTranslator: Function }) {
         this.tableSlider = params.data;
+        if (params.updateTranslatorTrigger) {
+            params.updateTranslatorTrigger.subscribe(function (newValue) {
+                params.updateTranslator();
+            });
+        }
+
+        if (params.updateTranslator) {
+            params.updateTranslator();
+        }
     }
 }
