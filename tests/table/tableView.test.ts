4c4dba20224b13cfb9ef3721d81892d35c6dcdd1
Andrey Kyrdyumov <kant2002@gmail.com>
2017-11-02 17:07:43 +0200

    Add no limit game test

--- tests/table/tableView.test.ts
+++ tests/table/tableView.test.ts
@@ -26,16 +26,81 @@ function getSeatPlayer(seat: number, initialAmount: number): PlayerStatusInfo {
 }
 
 describe("Table view", () => {
-    describe("Pot limit", () => {
+    describe("No limit", () => {
         beforeAll(() => {
             GameActionsQueue.waitDisabled = true;
             GameActionsQueue.drainQueuePause = 0;
-            GameActionsQueue.logging = true;
         });
         afterAll(() => {
             GameActionsQueue.waitDisabled = false;
             GameActionsQueue.drainQueuePause = 100;
-            GameActionsQueue.logging = false;
+        });
+        it("Pot limit on preflop", async () => {
+            global.messages = {
+            };
+            const tableView = new TableView(1, {
+                TableId: 1,
+                TableName: "",
+                BigBlind: 200,
+                SmallBlind: 100,
+                CurrencyId: 1,
+                HandsPerHour: 0,
+                AveragePotSize: 0,
+                JoinedPlayers: 2,
+                MaxPlayers: 8,
+                PotLimitType: 2,
+            });
+            const players: GamePlayerStartInformation[] = [{
+                PlayerId: 1,
+                Money: 10000,
+            }, {
+                PlayerId: 2,
+                Money: 10000,
+            }, {
+                PlayerId: 3,
+                Money: 10000,
+            }, {
+                PlayerId: 4,
+                Money: 10000,
+            }];
+            const actions: GameActionStartInformation[] = [];
+            login("player1");
+            loginId(1);
+            const tableSatusPlayers = [
+                getSeatPlayer(1, 10000),
+                getSeatPlayer(2, 10000),
+                getSeatPlayer(3, 10000),
+                getSeatPlayer(4, 10000),
+            ];
+            tableView.onTableStatusInfo(tableSatusPlayers, [], null, 4, 100, 10, null, null, null, null, null, true, 0, false, true, null, 0, 1);
+            tableView.onGameStarted(1, players, actions, 4);
+            tableView.onBet(1, 0, 100, 2);
+            tableView.onBet(2, 0, 200, 3);
+            tableView.onPlayerCards(1, [1, 2]);
+            tableView.onPlayerCards(2, [1, 2]);
+            tableView.onPlayerCards(3, [1, 2]);
+            tableView.onPlayerCards(4, [1, 2]);
+            await tableView.queue.waitCurrentTask();
+            while (tableView.queue.size() > 0) {
+                await tableView.queue.execute();
+                await tableView.queue.waitCurrentTask();
+            }
+
+            const currentPlayer = tableView.currentPlayer();
+            expect(currentPlayer).not.toBeNull();
+            expect(currentPlayer.PlayerId()).toBe(3);
+            expect(tableView.minimumRaiseAmount()).toBe(400);
+            expect(tableView.maximumRaiseAmount()).toBe(10000);
+        });
+    });
+    describe("Pot limit", () => {
+        beforeAll(() => {
+            GameActionsQueue.waitDisabled = true;
+            GameActionsQueue.drainQueuePause = 0;
+        });
+        afterAll(() => {
+            GameActionsQueue.waitDisabled = false;
+            GameActionsQueue.drainQueuePause = 100;
         });
         it("Pot limit on preflop", async () => {
             global.messages = {
@@ -76,8 +141,8 @@ describe("Table view", () => {
             ];
             tableView.onTableStatusInfo(tableSatusPlayers, [], null, 4, 100, 10, null, null, null, null, null, true, 0, false, true, null, 0, 2);
             tableView.onGameStarted(1, players, actions, 4);
-            tableView.onBet(1, 2, 100, 2);
-            tableView.onBet(2, 2, 200, 3);
+            tableView.onBet(1, 0, 100, 2);
+            tableView.onBet(2, 0, 200, 3);
             tableView.onPlayerCards(1, [1, 2]);
             tableView.onPlayerCards(2, [1, 2]);
             tableView.onPlayerCards(3, [1, 2]);
