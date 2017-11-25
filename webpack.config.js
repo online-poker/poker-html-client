e5e1572677953a310e48349da5d89c400862ce67
Andrey Kyrdyumov <kant2002@gmail.com>
2017-11-25 12:34:26 +0600

    Add Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+const bundleOutputDir = './dist';
+
+module.exports = (env) => {
+    const isDevBuild = !(env && env.prod);
+    return [{
+        stats: { modules: false },
+        entry: { 'main': './js/appInit' },
+        resolve: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            alias: {
+                "app": path.join(__dirname, "js"),
+            }
+        },
+        externals: {
+            jquery: 'jquery',
+            moment: 'moment',
+            knockout: 'knockout',
+            signals: 'signals',
+            signalr: 'signalr',
+            'knockout.validation': 'knockout.validation',
+        },
+        output: {
+            path: path.join(__dirname, bundleOutputDir),
+            filename: '[name].js',
+            publicPath: 'dist/',
+            library: {
+                root: "PokerClient",
+                amd: 'poker-html-client',
+                commonjs: 'poker-html-client',
+            },
+            libraryTarget: "umd",
+            umdNamedDefine: true            
+        },
+        module: {
+            rules: [
+                { test: /\.(tsx|ts)?$/, include: /js/, use: 'awesome-typescript-loader?silent=true' },
+                { test: /\.css$/, use: isDevBuild ? ['style-loader', 'css-loader'] : ExtractTextPlugin.extract({ use: 'css-loader?minimize' }) },
+                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+            ]
+        },
+        plugins: [
+            new CheckerPlugin(),
+        ].concat(isDevBuild ? [
+            // Plugins that apply in development builds only
+            new webpack.SourceMapDevToolPlugin({
+                filename: '[file].map', // Remove this line if you prefer inline source maps
+                moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
+            })
+        ] : [
+                // Plugins that apply in production builds only
+                new webpack.optimize.UglifyJsPlugin(),
+                new ExtractTextPlugin('site.css')
+            ])
+    }];
+};
