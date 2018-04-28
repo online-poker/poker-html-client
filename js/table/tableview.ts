ca0a07b59eff5287db9478ad9b48b8b069554a11
Andrii Kurdiumov <kant2002@gmail.com>
2018-04-28 18:44:25 +0600

    Add tests for time pass calculations

--- js/table/tableview.ts
+++ js/table/tableview.ts
@@ -376,7 +376,7 @@ export class TableView {
 
             let pass = self.timePass();
             pass = pass === null ? 0 : pass;
-            const moveTime = appConfig.timeSettings.moveTime | runtimeSettings.game.moveTime;
+            const moveTime = appConfig.timeSettings.moveTime || runtimeSettings.game.moveTime;
             return moveTime - pass;
         });
         this.currentRaise = ko.computed<number>({
