87757831db31437d9e9ffb211cdb7bf521492713
m.tutynina <m.tutynina@list.ru>
2018-04-28 17:50:35 +0600

    Add move time parameter to app configuration.

--- js/appconfig.ts
+++ js/appconfig.ts
@@ -17,6 +17,10 @@ class GameActionBlock {
     public hasSecondaryPanel = true;
 }
 
+export interface TimeSettings {
+    moveTime: undefined | number;
+}
+
 export class AppConfig {
     public auth = {
         automaticLogin: true,
@@ -66,6 +70,9 @@ export class AppConfig {
         hasInfoPages: false,
         hasSupportPages: false,
     };
+    public timeSettings: TimeSettings = {
+        moveTime: undefined,
+    };
 }
 /**
  * Simple object check.
@@ -87,12 +94,12 @@ function mergeDeep(target: any, ...sources: any[]): any {
 
     if (isObject(target) && isObject(source)) {
         for (const key in source) {
-        if (isObject(source[key])) {
-            if (!target[key]) { Object.assign(target, { [key]: {} }); }
-            mergeDeep(target[key], source[key]);
-        } else {
-            Object.assign(target, { [key]: source[key] });
-        }
+            if (isObject(source[key])) {
+                if (!target[key]) { Object.assign(target, { [key]: {} }); }
+                mergeDeep(target[key], source[key]);
+            } else {
+                Object.assign(target, { [key]: source[key] });
+            }
         }
     }
 
