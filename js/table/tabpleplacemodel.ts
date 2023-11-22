a19fb726fc95912cc622f698ee90e052c135b3c0
Andrii Kurdiumov <kant2002@gmail.com>
2023-11-22 12:30:08 +0600

    Provide property for hosting embedded seat (#270)
    For local development we will use consequetive ports.
    Table will use base port, and all other ports allocated for seats.
    For staging and production let's use naming convention for naming `seat1.domain.com`, `seatN.domain.com`.

--- js/table/tabpleplacemodel.ts
+++ js/table/tabpleplacemodel.ts
@@ -150,6 +150,11 @@ export class TablePlaceModel {
     public needCardsOverlay = ko.observable(false);
 
     /**
+     * Address of the emebedded seat.
+     */
+    public embeddedSeatAddress: ko.Computed<string>;
+
+    /**
      * Timer which control clearing of current action text
      */
     private CurrentActionTimer: number = 0;
@@ -262,6 +267,13 @@ export class TablePlaceModel {
 
             return this.LastChatMessage().substr(0, appConfig.game.messageDisplaySize || 17) + "\u2026";
         });
+        this.embeddedSeatAddress = ko.computed(() => {
+            if (window.location.host.indexOf("localhost")) {
+                return "//" + window.location.host + ":" + (window.location.port + this.Seat()) + "/embedded/seat";
+            }
+
+            return "//seat" + this.Seat() + "." + window.location.host + "/embedded/seat";
+        }, this);
     }
     public prepareForNewGame() {
         this.TotalBet(null);
