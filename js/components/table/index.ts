005ed78c6befd3713fdf2c8844cf0b8fbec1476c
Andrii Kurdiumov <kant2002@gmail.com>
2025-01-04 19:16:42 +0500

    Add quick table menu boilerplate

--- js/components/table/index.ts
+++ js/components/table/index.ts
@@ -1,3 +1,4 @@
 export { ActionBlockComponent } from "./actionBlock/actionBlock";
 export { TableMenuComponent } from "./menu/menu";
+export { QuickTableMenuComponent } from "./quick-menu/quick-menu";
 export { RaiseBlockComponent } from "./raiseBlock/raiseBlock";
