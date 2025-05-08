14bb0c6dddb4b1fad8e480a74e27a781e4fd02bb
Andrii Kurdiumov <kant2002@gmail.com>
2025-05-08 22:44:43 +0500

    Hide buttons deposit and history when player not in game (#328)
    * Hide buttons deposit and history when player not in game
    
    * Fix compilation error

--- js/components/table/quick-menu/quick-menu.ts
+++ js/components/table/quick-menu/quick-menu.ts
@@ -1,15 +1,24 @@
-﻿import { Observable, observable } from "knockout";
+﻿import { Computed, Observable, observable } from "knockout";
 import { TablesPage } from "../../../pages/tablespage";
 import { App } from "poker/app";
+import ko = require("knockout");
 
 declare const app: App;
 
 export class QuickTableMenuComponent {
     private page: TablesPage;
     private visible: Observable<boolean> = observable(false);
+    public couldDeposit: Computed<boolean>;
+    public depositVisible: Computed<boolean>;
+    public handHistoryEnabled : Computed<boolean>;
+    public handHistoryVisible: Computed<boolean>;
 
     constructor(params: { data: TablesPage }) {
         this.page = params.data;
+        this.couldDeposit = ko.pureComputed(() => this.page.currentTable().couldAddChips());
+        this.depositVisible = ko.pureComputed(() => this.page.currentTable().addMoneyAvailable() && this.page.currentTable().myPlayer() != null);
+        this.handHistoryEnabled = ko.pureComputed(() => this.page.currentTable().handHistoryAllowed());
+        this.handHistoryVisible = ko.pureComputed(() => this.page.currentTable().myPlayer() != null);
     }
 
     public toggleMenu() {
